68dcdf757e6cb30a57c3770697c379a5ed765021
alshamlan <malsh002@fiu.edu>
2014-09-26 12:26:39 -0400

    basic read() syscall example

---
+++ basic_linux_syscall_file_IO/ex1_basic_read_syscall.c
@@ -0,0 +1,67 @@
+/***********************************************************************
+* Code listing from "Advanced Linux Programming," by CodeSourcery LLC  *
+* Copyright (C) 2001 by New Riders Publishing                          *
+* See COPYRIGHT for license information.                               *
+***********************************************************************/
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+/* Read the contents of FILENAME into a newly-allocated buffer.  The
+   size of the buffer is stored in *LENGTH.  Returns the buffer, which
+   the caller must free.  If FILENAME doesn't correspond to a regular
+   file, returns NULL.  */
+
+char* read_file (const char* filename, size_t* length)
+{
+  int fd;
+  struct stat file_info;
+  char* buffer;
+
+  /* Open the file.  */
+  fd = open (filename, O_RDONLY);
+
+  /* Get information about the file.  */
+  fstat (fd, &file_info);
+  *length = file_info.st_size;
+  /* Make sure the file is an ordinary file.  */
+  if (!S_ISREG (file_info.st_mode)) {
+    /* It's not, so give up.  */
+    close (fd);
+    return NULL;
+  }
+
+  /* Allocate a buffer large enough to hold the file's contents.  */
+  buffer = (char*) malloc (*length);
+  /* Read the file into the buffer.  */
+  read (fd, buffer, *length);
+
+  /* Finish up.  */
+  close (fd);
+  return buffer;
+}
+
+int main(int argc, char *argv[])
+{
+	if(argc < 2){
+		perror("you are missing command-line argument\nPlease, include the file name\n");
+		exit(EXIT_FAILURE);
+	}
+	//
+	size_t len;
+	char *str=read_file(argv[1], &len);
+	if(str==NULL){
+		perror("something wrong!!\n");
+		exit(EXIT_FAILURE);		
+	}
+	//
+	printf("the string\n---------------------\n%s", str);
+	printf("the length\n---------------------\n%zu\n", len);
+	//
+	free(str);
+	return 0;	
+}
